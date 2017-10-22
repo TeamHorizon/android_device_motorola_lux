4828343eb468833a8d042a809270ca5bcbcb9c0f
axxx007xxxz <mattiabrsrn7@gmail.com>
2017-10-22 15:48:19 +0200

    lux: Build NFC service HAL

--- device.mk
+++ device.mk
@@ -52,6 +52,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
+    android.hardware.nfc@1.0-service \
     libnfc \
     libnfc_jni \
     nfc_nci.bcm2079x.default \
